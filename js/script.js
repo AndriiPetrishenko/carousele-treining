ceb5bb73b320aba09ad5235139e42ebc459e847e
Andrii Petrishenko <anderrzen@gmail.com>
2021-05-20 00:57:59 +0300

    убрал счетчик

--- js/script.js
+++ js/script.js
@@ -74,29 +74,29 @@ $(document).ready(function () {
 
 //счетчик
 
-var $slider = $('.sliderbig');
+// var $slider = $('.sliderbig');
 
-if ($slider.length) {
-  var currentSlide;
-  var slidesCount;
-  var sliderCounter = document.createElement('div');
-  sliderCounter.classList.add('slider__counter');
+// if ($slider.length) {
+//   var currentSlide;
+//   var slidesCount;
+//   var sliderCounter = document.createElement('div');
+//   sliderCounter.classList.add('slider__counter');
   
-  var updateSliderCounter = function(slick, currentIndex) {
-    currentSlide = slick.slickCurrentSlide() + 1;
-    slidesCount = slick.slideCount;
-    $(sliderCounter).text(currentSlide + '/' +slidesCount)
-  };
+//   var updateSliderCounter = function(slick, currentIndex) {
+//     currentSlide = slick.slickCurrentSlide() + 1;
+//     slidesCount = slick.slideCount;
+//     $(sliderCounter).text(currentSlide + '/' +slidesCount)
+//   };
 
-  $slider.on('init', function(event, slick) {
-    $slider.append(sliderCounter);
-    updateSliderCounter(slick);
-  });
+//   $slider.on('init', function(event, slick) {
+//     $slider.append(sliderCounter);
+//     updateSliderCounter(slick);
+//   });
 
-  $slider.on('afterChange', function(event, slick, currentSlide) {
-    updateSliderCounter(slick, currentSlide);
-  });
+//   $slider.on('afterChange', function(event, slick, currentSlide) {
+//     updateSliderCounter(slick, currentSlide);
+//   });
 
-  $slider.slick();
-}
+//   $slider.slick();
+// }
 
